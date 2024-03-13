0fc305b1dea45406ccbf4b74924798548159159f
dincer4949 <udincer4949@student.pps.net>
2024-03-13 12:02:56 -0700

    finally mysql added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,21 @@
 import java.sql.*;
-import org.postgresql.*;
 
 public class Main {
 
     static String url = "jdbc:postgresql://localhost:5432/postgres"; // table details
     static String username = "postgres";
     static String password = "dincer1";
+    //MySql infos--------------------------------------------------------
+    static String myUrl = "jdbc:mysql://sql3.freesqldatabase.com/sql3690872"; // table details
+    static String myUser = "sql3690872";
+    static String myPass = "dtEtTbcvvi";
     public static void main(String[] args) {
 
         try{
-            Connection con = DriverManager.getConnection(url, username, password);
+           // Connection con = DriverManager.getConnection(url, username, password);
+            Connection con = DriverManager.getConnection(myUrl, myUser, myPass);
 
-            PreparedStatement prestmt = con.prepareStatement("SELECT * FROM users");
+            PreparedStatement prestmt = con.prepareStatement("SELECT * FROM crud_test");
             ResultSet rs = prestmt.executeQuery();
 
             while(rs.next()){
