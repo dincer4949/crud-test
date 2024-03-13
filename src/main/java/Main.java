550b0e3aa271204b1fc7c560330f237680726e25
dincer4949 <udincer4949@student.pps.net>
2024-03-13 09:15:49 -0700

    frist-and-test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.sql.*;
+
 public class Main {
+
+    static String url = "jdbc:postgresql://localhost:5432/users"; // table details
+    static String username = "postgres";
+    static String password = "dincer1";
     public static void main(String[] args) {
-        System.out.println("asdasd");
+        getConnection();
+
+    }
+
+
+    public static void getConnection() {
+
+        try{
+            Connection con = DriverManager.getConnection(url, username, password);
+            String getTable = "SELECT * FROM users";
+            PreparedStatement prestmt = con.prepareStatement(getTable);
+            ResultSet rs = prestmt.executeQuery();
+
+            while(rs.next()){
+                //main get columns
+            }
+            // use con here
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
